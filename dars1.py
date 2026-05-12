a89ec159fc4f4f5b1fc0c334d07cc4ee0c9c8fbe
ismoilkarimov21 <ismoilkarimov993@gmail.com>
2026-05-12 13:50:14 +0500

    Create dars1.py

---
+++ dars1.py
@@ -0,0 +1,34 @@
+# https://core.telegram.org/bots/api#sendmessage
+import requests
+token = "8791160751:AAFAxS-vflirjyAzgIdVay1jEdICSRfv3_E"
+method = 'forwardMessage'
+response = requests.post(
+        url=f'https://api.telegram.org/bot{token}/{method}',
+        data={'chat_id': 6667845769, 'from_chat_id':7813037554, 'message_id':2}
+    ).json()
+print(response)
+
+
+method = 'copyMessage'
+response = requests.post(
+        url=f'https://api.telegram.org/bot{token}/{method}',
+        data={'chat_id': 392330197, 'from_chat_id': , 'message_id': }
+    ).json()
+print(response)
+
+
+token = "8791160751:AAFAxS-vflirjyAzgIdVay1jEdICSRfv3_E"
+url = f'https://telegram.org{token}/sendPhoto'
+photo_path = r'C:\Users\user\Downloads\bird.jpg'
+
+with open(photo_path, 'rb') as photo_file:
+    payload = {
+        'chat_id': 6667845769,
+        'caption': 'I like Birds'
+    }
+    files = {
+        'photo': photo_file
+    }
+
+    response = requests.post(url, data=payload, files=files).json()
+print(response)
